e8ce6f5833529584ae58d2611155ba64791c739a
WillChomrak <45401483+WillChomrak@users.noreply.github.com>
2022-10-14 15:06:48 -0400

    Update and rename OrganizeImages.py to organizeimages.py

--- organizeimages.py
+++ organizeimages.py
@@ -3,7 +3,7 @@ from fastai.imports import *
 # This is a great little package for splitting your images into a structure your model can digest
 !pip install split-folders[full]
 
-# You need to connect to Google Collab if that is where your images are
+# You need to connect to Google Drive if that is where your images are
 from google.colab import drive
 drive.mount('/content/gdrive')
 
